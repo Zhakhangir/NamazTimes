83ae61306fcbdc0ec3fe8a5a625d752b0256d9f9
Жахангир Таиров <jahangirtairov123@gmail.com>
2022-04-19 04:45:44 +0600

    added app icon, removed main storyboard, ios version: 10.0

---
+++ NamazTimes/Modules/General view controller/GeneralViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralViewController.swift
+//  NamazTimes
+//
+//  Created by &&TairoV on 4/17/22.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
